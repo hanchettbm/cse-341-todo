2e852508b9e0ce11cc5f96c2a1dc13fbdd2eaf2f
Baden <hanchettbm@gmail.com>
2022-04-30 19:12:19 -0600

    Added contacts route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-  res.send('Samantha Jarnagin');
-});
+// routes.get('/', (req, res) => {
+//   res.send('Samantha Jarnagin');
+// });
+
+routes.use('/', require('./home'));
+
+routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -7,18 +7,3 @@ app.use('/', require('./routes'))
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
-
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("contacts");
-    dbo.collection("contacts").find().toArray(function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
-});
