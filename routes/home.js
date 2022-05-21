18ff502f004f79bbbc591c4cb9822053042cd032
Baden <hanchettbm@gmail.com>
2022-05-20 18:16:45 -0600

    home

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-  res.send('Hello! you`re Home!');
+  res.send("Hello! you're Home!");
 });
 
 module.exports = routes;
