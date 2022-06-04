c6e9f3889cd877aad36ce0e340fe51730a97066d
Baden <hanchettbm@gmail.com>
2022-06-04 17:29:40 -0600

    some user stuff

--- config/passport.js
+++ config/passport.js
@@ -5,10 +5,30 @@ module.exports = function (passport) {
     passport.use(new GoogleStratagey({
         clientID: '123600961674-qd0l5dgnhnoe627h3nva3ou88c3268ul.apps.googleusercontent.com',
         clientSecret: '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',
-        callbackURL: '/auth/google/callback' 
+        callbackURL: 'http://localhost:3000/auth/google/callback' 
     },
     async (accessToken, refreshToken, profile, done) => {
         console.log(profile);
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try {
+            let user = await User.findOne({ googleId: profile.id })
+
+            if (user) {
+                done(null, user)
+            } else {
+                user = await User.create(newUser);
+                done(null, user);
+            }
+        } catch (err){
+            console.error(err);
+        }
 
     }));
 
